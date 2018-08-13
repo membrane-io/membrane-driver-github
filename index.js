827a58fa8ce8e9737d8f0abe4596e195f90225ea
Me <me@example.com>
2018-08-13 23:13:50 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -206,8 +206,7 @@ export const Issue = {
   activeLockReason({ source }) { return source['active_lock_reason']; },
   async subscribe({self, source}){
     const { id } = await self.id.$query()
-    console.log(id);
-    //await client.activity.setNotificationThreadSubscription({thread_id: id})
+    await client.activity.setNotificationThreadSubscription({thread_id: id})
   },
 }
 
