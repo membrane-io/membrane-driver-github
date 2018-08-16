f0bb622622e459a6f26a9b29be530592256b6263
Me <me@example.com>
2018-08-16 15:19:06 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -315,7 +315,10 @@ export async function timer({ key }) {
 async function ensureTimerIsSet(repo, event){
   const { state } = program;
   
-  const repository =  state.events[`${repo}`] ?  state.events[`${repo}`] : [];
+  
+  state.events[repo] || [];
+  
+  const repository = state.events[repo];
 
   if(repository.length === 0){
     await program.setTimer(repo, 0, 10);
