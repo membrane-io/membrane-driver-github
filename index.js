9fbd0acff457368b9f9c27af195c449c0baab390
Me <me@example.com>
2018-08-17 01:58:18 +0000

    Commit made from Membrane web

--- index.js
+++ index.js
@@ -292,7 +292,7 @@ export async function timer({ key }) {
 
   const data = result.data
     .filter(item => formatTime(item.created_at) <= state.repos[key].lastEventTime)
-
+  console.log("DATA" + JSON.stringify(data))
     for (let event of state.repos[key].events) {
       switch (event) {
         case 'issueOpened': {
