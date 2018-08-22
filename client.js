a318ab16123c625ad52bc98e0b29fa55ec19460c
aranajhonny <jhonnyjosearana@gmail.com>
2018-08-21 20:25:35 -0430

    add release and run prettier

--- client.js
+++ client.js
@@ -1,45 +1,41 @@
-import GithubApi from '@octokit/rest';
-import axios from 'axios';
+import GithubApi from "@octokit/rest";
+import axios from "axios";
 
 let { ACCESS_TOKEN } = process.env;
 if (!ACCESS_TOKEN) {
-  throw new Error('Please provide ACCESS_TOKEN as an environment variable');
+  throw new Error("Please provide ACCESS_TOKEN as an environment variable");
 }
 
 let { url: webhookUrl } = program.endpoints.webhook;
 if (!webhookUrl) {
-  throw new Error('Failed to determine webhook URL');
+  throw new Error("Failed to determine webhook URL");
 }
 
-export const client = new GithubApi({
-});
+export const client = new GithubApi({});
 
 client.authenticate({
-  type: 'token',
-  token: ACCESS_TOKEN,
-})
-
- 
-
+  type: "token",
+  token: ACCESS_TOKEN
+});
 
 export async function getDiff(url, params) {
   const instance = axios.create({
     headers: {
-    'Content-Type': 'Accept: application/vnd.github.diff',
-    Authorization: 'token ' + ACCESS_TOKEN,
+      "Content-Type": "Accept: application/vnd.github.diff",
+      Authorization: "token " + ACCESS_TOKEN
     }
   });
   const result = await instance.get(url, { params });
   return result.data;
 }
 // TODO: axios client
-export async function graphql(query, variables){
+export async function graphql(query, variables) {
   const body = {
-      query: query,
-      variables: variables
-    }
-    const client = axios.create({
-      headers: {'Authorization': `token ${process.env.ACCESS_TOKEN}`}
-    });
-    await client.post(`https://api.github.com/graphql`, body)
+    query: query,
+    variables: variables
+  };
+  const client = axios.create({
+    headers: { Authorization: `token ${process.env.ACCESS_TOKEN}` }
+  });
+  await client.post(`https://api.github.com/graphql`, body);
 }
